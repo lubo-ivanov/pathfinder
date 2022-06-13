5e9e4b9509e0ed6d88b4ba5258afd2f4257ad1ab
Lubo <wulu@abv.bg>
2022-06-14 00:01:06 +0300

    added lombok dependency and implementation

--- src/main/java/softuni/pathfinder/model/entity/Role.java
+++ src/main/java/softuni/pathfinder/model/entity/Role.java
@@ -1,24 +1,26 @@
 package softuni.pathfinder.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import softuni.pathfinder.model.entity.enums.UserRoles;
 
 import javax.persistence.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "roles")
-public class Role extends BaseEntity{
-    private UserRoles name;
-
-    public Role() {
-    }
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    public UserRoles getName() {
-        return name;
-    }
+    private UserRoles role;
 
-    public void setName(UserRoles name) {
-        this.name = name;
-    }
 }
--- src/main/java/softuni/pathfinder/model/entity/User.java
+++ src/main/java/softuni/pathfinder/model/entity/User.java
@@ -1,68 +1,38 @@
 package softuni.pathfinder.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import softuni.pathfinder.model.entity.enums.Level;
 
 import javax.persistence.*;
 import java.util.Set;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "users")
 public class User extends BaseEntity {
-    private String username;
-    //o Accepts values, which should be at least 2 characters
-    private String password;
-    //o Accepts values, which should be at least 2 characters
-    private String email;
-    //o Accepts values, which contain the '@' symbol
-    private Set<Role> roles;
-    //o Each registered user should have a "User" role
-    private Level level;
-
-    public User() {
-    }
 
     @Column(unique = true, nullable = false)
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    private String username;
+    //o Accepts values, which should be at least 2 characters
 
     @Column(nullable = false)
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    private String password;
+    //o Accepts values, which should be at least 2 characters
 
     @Column(unique = false, nullable = false)
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String email;
+    //o Accepts values, which contain the '@' symbol
 
     @ManyToMany
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
+    private Set<Role> roles;
+    //o Each registered user should have a "User" role
 
     @Enumerated(EnumType.STRING)
-    public Level getLevel() {
-        return level;
-    }
-
-    public void setLevel(Level level) {
-        this.level = level;
-    }
+    private Level level;
 }
