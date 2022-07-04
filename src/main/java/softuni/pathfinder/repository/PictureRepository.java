37a4da02534fc9bf1b55aaa83a9cef021805dc7f
Lubo <wulu@abv.bg>
2022-07-04 18:59:23 +0100

    refactored and implemented about and added dynamic pics

---
+++ src/main/java/softuni/pathfinder/repository/PictureRepository.java
@@ -0,0 +1,15 @@
+package softuni.pathfinder.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import softuni.pathfinder.model.entity.Picture;
+
+import java.util.List;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+
+    @Query("SELECT p.url FROM Picture p")
+    List<String> findAllUrls();
+}
--- src/main/java/softuni/pathfinder/web/controllers/HomeController.java
+++ src/main/java/softuni/pathfinder/web/controllers/HomeController.java
@@ -1,14 +1,21 @@
 package softuni.pathfinder.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import softuni.pathfinder.service.PictureService;
 
 @Controller
 public class HomeController {
+private final PictureService pictureService;
 
+    public HomeController(PictureService pictureService) {
+        this.pictureService = pictureService;
+    }
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("picturesList", pictureService.findAllUrls());
         return "index";
     }
 
